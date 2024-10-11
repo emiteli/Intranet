2e9a097c2b01ead88324f64c8b6c1075e3b70a25
emiteli <ti@emiteli.com.br>
2024-10-11 12:06:13 -0300

    app modificado

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from extensions import db, login_manager, migrate
 
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
@@ -8,6 +9,10 @@ db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'  # Alterado aqui
 migrate.init_app(app, db)
+
+
+with app.app_context():
+    db.create_all()
 
 from routes.routes import routes
 app.register_blueprint(routes)
