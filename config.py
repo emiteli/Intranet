aa0fc94a590654c4dd11847e6804d88effd3d421
emiteli <ti@emiteli.com.br>
2024-10-11 11:07:25 -0300

    update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'Emiteli@123'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:user_password@db/meu_banco'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:user_password@db/intranet'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
     DEBUG = True
